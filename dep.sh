a403fffe37ce567f8e833f2d89e26f83f9dce5d1
logon84 <logon84@users.noreply.github.com>
2018-04-28 00:03:05 +0200

    Update dep.sh

--- dep.sh
+++ dep.sh
@@ -13,6 +13,7 @@ opkg upgrade
 opkg install libusb-1.0
 opkg install libusb-compat
 opkg install python3
+opkg install python3-pip
 pip3 install --upgrade setuptools
 pip3 install libusb1
 pip3 install pyusb
