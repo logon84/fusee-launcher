5a0f815da2457887ae752452db4089fc4388409f
logon84 <logon84@users.noreply.github.com>
2018-04-27 18:16:01 +0200

    DD-WRT dependencies
    DD-WRT dependencies

--- dep.sh
+++ dep.sh
@@ -1,18 +1,18 @@
 #!/bin/sh
 
+#install ENTWARE
 wget -O - http://pkg.entware.net/binaries/armv7/installer/entware_install.sh | sh
+
+#update ENTWARE
 opkg update
 opkg upgrade
 opkg update
 opkg upgrade
-opkg install nano
+
+#Needed python & USB stuff
 opkg install libusb-1.0
-
 opkg install libusb-compat
-
 opkg install python3
 pip3 install --upgrade setuptools
-
 pip3 install libusb1
-
 pip3 install pyusb
